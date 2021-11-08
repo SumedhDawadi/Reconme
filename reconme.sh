837637fc821500c56fbf749610ae3aeeff6f8a72
SumedhDawadi <57694660+SumedhDawadi@users.noreply.github.com>
2021-11-08 20:58:01 +0545

    Update reconme.sh

--- reconme.sh
+++ reconme.sh
@@ -26,21 +26,10 @@ if [[ $(id -u) != 0 ]]; then
     echo -e "\n[!] Install.sh requires root privileges"
     exit 0
 fi
-echo "Tools that will be installed in your system are : "
-printf "${BLUE}\n"
-echo "GO-land"
-echo "Nuclei"
-echo "Sublist3r"
-echo "Assetfinder"
 
 printf "${GREEN}\n"
 
-mkdir /opt/tool
-echo "done"
 
-#Installing Go-Language
-mkdir /opt/tool
-echo "done"
 
 #Installing Go-Language
 
@@ -53,7 +42,7 @@ select choice in "${choices[@]}"; do
                 yes)
 
                                         echo "Installing Golang"
-                                        wget https://dl.google.com/go/https://golang.org/dl/go1.17.2.linux-amd64.tar.gz
+                                        wget https://dl.google.com/go/https://golang.org/dl/go1.17.3.linux-amd64.tar.gz  
                                         sudo tar -xvf https://golang.org/dl/go1.17.2.linux-amd64.tar.gz
                                         sudo mv go /usr/local
                                         export GOROOT=/usr/local/go
@@ -76,32 +65,4 @@ done
 fi
 
 
-echo "Checking if the Tool called Nuclei is installed in your system or not"
-echo
-if ! command -v nuclei &> /dev/null
-then
-        GO111MODULE=on go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
-else
-echo
-        echo "Nuclie is already installed in your system."
 
-fi
-echo
-echo "Checking if the Sublist3r is installed in your system or not"
-if ! command -v sublist3r &> /dev/null
-then
-        git clone  https://github.com/aboul3la/Sublist3r.git && cd Sublist3r && python3 setup.py install &&cd $OLDPWD && rm -rf Sublist3r
-
-else
-        echo "Sublist3r is alreay installed in your system"
-
-fi
-
-
-echo "Checking if assertfinder is installed in your system or not"
-if ! command -v assetfinder &> /dev/null
-then
-        go get -u github.com/tomnomnom/assetfinder && cp assetfinder
-else
-        echo "Assetfinder is alreay installed in your system"
-fi
